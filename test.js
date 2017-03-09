16d39af66efe8f1c17bba387cf6980370dabdd85
Eliot Lash <fadookie@gmail.com>
2017-03-08 20:24:21 -0800

    make tests exit nonzero

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ const targz = require('tar.gz2');
 
 targz().extract(process.argv[2], 'data')
     .then(console.log('extract finished!!'))
-    .catch((err) => console.error('error callback fired: ', err));
+    .catch((err) => {
+        console.error('error callback fired: ', err)
+        process.exit(1);
+    });
 
 console.log('done?');
